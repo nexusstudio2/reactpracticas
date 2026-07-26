755b656c9347988f1de847ec9e06f7a37bedd23b
nexusstudio2 <angelsot801@gmail.com>
2026-07-25 19:52:26 -0600

    Día 7: contador con useState y useEffect

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { Button, Card, Typography } from "@mui/material";
+
+function App() {
+  const [contador, setContador] = useState(0);
+
+  useEffect(() => {
+    console.log("El contador cambió a:", contador);
+  }, [contador]);
+
+  const incrementar = () => {
+    setContador(contador + 1);
+  };
+
+  return (
+    <Card
+      style={{
+        padding: "20px",
+        textAlign: "center",
+        width: "300px",
+        margin: "50px auto",
+      }}
+    >
+      <Typography variant="h4">{contador}</Typography>
+      <Button variant="contained" onClick={incrementar}>
+        Incrementar
+      </Button>
+    </Card>
+  );
+}
+
+export default App;
